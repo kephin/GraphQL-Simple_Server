cfd51719e29ee76ae96d2c6b653f16078693080c
kephin <wunshon76325@gmail.com>
2018-04-08 13:56:09 +0800

    feat: create/delete/update patients

--- schema.js
+++ schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = require('graphql');
 
 // Patient Type
@@ -44,6 +45,49 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutation
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    createPatient: {
+      type: PatientType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      async resolve(parentValue, args){
+        const newPatient = await axios.post('http://localhost:3000/patients', { ...args });
+        return newPatient.data;
+      },
+    },
+    deletePatient: {
+      type: PatientType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      async resolve(parentValue, args){
+        const patient = await axios.delete(`http://localhost:3000/patients/${args.id}`);
+        return patient.data;
+      },
+    },
+    updatePatient: {
+      type: PatientType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      async resolve(parentValue, args) {
+        const patient = await axios.patch(`http://localhost:3000/patients/${args.id}`, args);
+        return patient.data;
+      },
+    }
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation
 });
